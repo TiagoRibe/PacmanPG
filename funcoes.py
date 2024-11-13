809aeaa6362c9bbd21e8f6222112a104421206fd
TiagoRibe <tiagocr1@al.insper.edu.br>
2024-11-13 09:30:05 -0300

    Tabuleiro finalizado e imagens colocadas

---
+++ funcoes.py
@@ -0,0 +1,47 @@
+import pygame
+import math
+from tabuleiro import tabuleiros
+
+
+LARGURA = 900
+ALTURA = 800
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+temporizador= pygame.time.Clock()
+fps= 60
+pygame.display.set_caption("Rato e Gatos - Menu")
+fonte = pygame.font.Font('freesansbold.ttf', 40)
+level= tabuleiros
+COR= 'blue'
+PI= math.pi
+COR_TEXTO = (255, 255, 0)
+COR_DESTAQUE = (100, 100, 255)
+
+def exibir_texto(texto, fonte, cor, posicao):
+    texto_superficie = fonte.render(texto, True, cor)
+    texto_retangulo = texto_superficie.get_rect(center=posicao)
+    tela.blit(texto_superficie, texto_retangulo)
+    return texto_retangulo  # Retorna o retângulo para detecção de clique
+
+def desenha_tabuleiro():
+    num1 = ((ALTURA - 50) // 32)
+    num2 = (LARGURA // 30)
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(tela, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+            if level[i][j] == 2 and not False:
+                pygame.draw.circle(tela, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+            if level[i][j] == 3:
+                pygame.draw.line(tela, COR, (j * num2 + (0.5 * num2), i * num1), (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(tela, COR, (j * num2, i * num1 + (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+            if level[i][j] == 5:
+                pygame.draw.arc(tela, COR, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1], 0, PI / 2, 3)
+            if level[i][j] == 6:
+                pygame.draw.arc(tela, COR, [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
+            if level[i][j] == 7:
+                pygame.draw.arc(tela, COR, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI, 3 * PI / 2, 3)
+            if level[i][j] == 8:
+                pygame.draw.arc(tela, COR, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2, 2 * PI, 3)
+            if level[i][j] == 9:
+                pygame.draw.line(tela, 'white', (j * num2, i * num1 + (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
--- tela.py
+++ tela.py
@@ -1,37 +1,33 @@
 import pygame
 import sys
 from tabuleiro import tabuleiros
+from funcoes import *
+import math
 
 # Inicialização do Pygame
 pygame.init()
 
 # configurando a tela inicial e cores
 LARGURA = 900
-ALTURA = 600
+ALTURA = 800
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 temporizador= pygame.time.Clock()
 fps= 60
 pygame.display.set_caption("Rato e Gatos - Menu")
 fonte = pygame.font.Font('freesansbold.ttf', 40)
 level= tabuleiros
-
-COR_FUNDO = (2, 13, 63)
+COR= 'blue'
+PI= math.pi
 COR_TEXTO = (255, 255, 0)
 COR_DESTAQUE = (100, 100, 255)
 
-
-# primeira função para exibir o texto na tela
-def exibir_texto(texto, fonte, cor, posicao):
-    texto_superficie = fonte.render(texto, True, cor)
-    texto_retangulo = texto_superficie.get_rect(center=posicao)
-    tela.blit(texto_superficie, texto_retangulo)
-    return texto_retangulo  # Retorna o retângulo para detecção de clique
 # loop principal para inicializar pygame
 rodando = True
 while rodando:
     temporizador.tick(fps)
-    
-    tela.fill(COR_FUNDO)
+    tela.fill('black')
+    #desenha_tabuleiro() 
+
     jogar_retangulo = exibir_texto("Jogar", fonte, COR_TEXTO, (LARGURA // 2, ALTURA // 2 - 50))
     sair_retangulo = exibir_texto("Sair", fonte, COR_TEXTO, (LARGURA // 2, ALTURA // 2 + 50))
     
