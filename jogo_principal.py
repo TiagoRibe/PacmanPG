0427b024cd826a9f0217ee6d2d94cc10160e6ac9
TiagoRibe <tiagocr1@al.insper.edu.br>
2024-11-17 19:42:48 -0300

    Adição de algumas funcoes

--- jogo_principal.py
+++ jogo_principal.py
@@ -15,9 +15,9 @@ fonte = pygame.font.Font('freesansbold.ttf', 20)
 nivel = copy.deepcopy(tabuleiros)
 cor = 'azul'
 PI = math.pi
-imagens_jogador = []
+fotos_rato = []
 for i in range(1, 5):
-    imagens_jogador.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45, 45)))
+    fotos_rato.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45, 45)))
 imagem_gato2 = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/red.png'), (45, 45))
 imagem_gato4 = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/pink.png'), (45, 45))
 imagem_gato3 = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/blue.png'), (45, 45))
@@ -652,3 +652,70 @@ class Fantasma:
         elif self.pos_x > 900:
             self.pos_x - 30
         return self.pos_x, self.pos_y, self.direcao
+    
+def desenha_variavel():
+    texto_pontuacao = fonte.render(f'Pontos: {pontuacao}', True, 'white')
+    tela.blit(texto_pontuacao, (10, 920))
+    if powerup_ativo:
+        pygame.draw.circle(tela, 'blue', (140, 930), 15)
+    for i in range(vidas):
+        tela.blit(pygame.transform.scale(fotos_rato[0], (30, 30)), (650 + i * 40, 915))
+    if jogo_encerrado:
+        pygame.draw.rect(tela, 'white', [50, 200, 800, 300],0, 10)
+        pygame.draw.rect(tela, 'dark gray', [70, 220, 760, 260], 0, 10)
+        gameover_text = fonte.render('Você perdeu! Aperte espaço para recomeçar!', True, 'red')
+        tela.blit(gameover_text, (100, 300))
+    if jogo_vencido:
+        pygame.draw.rect(tela, 'white', [50, 200, 800, 300],0, 10)
+        pygame.draw.rect(tela, 'dark gray', [70, 220, 760, 260], 0, 10)
+        gameover_text = fonte.render('Parabens, você venceu! Aperte espaço para recomeçar!', True, 'green')
+        tela.blit(gameover_text, (100, 300))
+
+def checa_colisao(pont, poder, conta_poder, gatos_comidos):
+    num1 = (ALTURA - 50) // 32
+    num2 = LARGURA // 30
+    if 0 < jogador_x < 870:
+        if nivel[centro_y // num1][center_x // num2] == 1:
+            nivel[centro_y // num1][center_x // num2] = 0
+            pont += 10
+        if nivel[centro_y // num1][center_x // num2] == 2:
+            nivel[centro_y // num1][center_x // num2] = 0
+            pont += 50
+            poder = True
+            conta_poder = 0
+            gatos_comidos = [False, False, False, False]
+    return pont, poder, conta_poder, gatos_comidos
+
+def desenha_tabuleiro():
+    num1 = ((ALTURA - 50) // 32)
+    num2 = (LARGURA // 30)
+    for i in range(len(nivel)):
+        for j in range(len(nivel[i])):
+            if nivel[i][j] == 1:
+                pygame.draw.circle(tela, 'white', (pos_x, pos_y), 4)
+            elif nivel[i][j] == 2:
+                pygame.draw.circle(tela, 'white', (pos_x, pos_y), 10)
+            elif nivel[i][j] == 3:
+                pygame.draw.line(tela, cor, (pos_x, i * num1), (pos_x, i * num1 + num1), 3)
+            elif nivel[i][j] == 4:
+                pygame.draw.line(tela, cor, (j * num2, pos_y), (j * num2 + num2, pos_y), 3)
+            elif nivel[i][j] == 5:
+                pygame.draw.arc(tela, cor, [(j * num2 - (num2 * 0.4)) - 2, pos_y, num2, num1], 0, PI / 2, 3)
+            elif nivel[i][j] == 6:
+                pygame.draw.arc(tela, cor, [(j * num2 + (num2 * 0.5)), pos_y, num2, num1], PI / 2, PI, 3)
+            elif nivel[i][j] == 7:
+                pygame.draw.arc(tela, cor, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI, 3 * PI / 2, 3)
+            elif nivel[i][j] == 8:
+                pygame.draw.arc(tela, cor, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2, 2 * PI, 3)
+            elif nivel[i][j] == 9:
+                pygame.draw.line(tela, 'white', (j * num2, pos_y), (j * num2 + num2, pos_y), 3)
+
+def desenha_jogador():
+    if direcao == 0:
+        tela.blit(fotos_rato[contador // 5], (jogador_x, jogador_y))
+    elif direcao == 1:
+        tela.blit(pygame.transform.flip(fotos_rato[contador // 5], True, False), (jogador_x, jogador_y))
+    elif direcao == 2:
+        tela.blit(pygame.transform.rotate(fotos_rato[contador // 5], 90), (jogador_x, jogador_y))
+    elif direcao == 3:
+        tela.blit(pygame.transform.rotate(fotos_rato[contador // 5], 270), (jogador_x, jogador_y))
